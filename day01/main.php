ea7565ba1d42db748f871e79ee149705382fdc3f
Marvin <marvin.mardiat@upitech.ee>
2022-12-02 16:57:06 +0200

    Day 1 half done

--- day01/main.php
+++ day01/main.php
@@ -1,1 +1,32 @@
 <?php
+
+require "vendor/autoload.php";
+
+use App\Utils;
+
+$inputs = Utils::getInput($argc, $argv);
+
+$topCalories = 0;
+$currentCalories = 0;
+$totalCaloriesPerElf = [0, 0, 0];
+foreach ($inputs as $calories) {
+    if (strlen(trim($calories)) === 0) {
+        if ($currentCalories > min($totalCaloriesPerElf)) {
+            $totalCaloriesPerElf[] = $currentCalories;
+        }
+
+        if ($topCalories < $currentCalories) {
+            $topCalories = $currentCalories;
+        }
+
+        $currentCalories = 0;
+
+        continue;
+    }
+
+    $currentCalories += (int) $calories;
+}
+
+echo $topCalories . PHP_EOL;
+
+
