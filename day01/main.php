d59acce6dbd6866ebae742a7862ee133a07da760
Marvin Mardiat <mardiat@hotmail.com>
2022-12-03 10:12:30 +0200

    Day 1 complete

--- day01/main.php
+++ day01/main.php
@@ -5,28 +5,43 @@ require "vendor/autoload.php";
 use App\Utils;
 
 $inputs = Utils::getInput($argc, $argv);
+$lastKey = array_key_last($inputs);
 
 $topCalories = 0;
 $currentCalories = 0;
-$totalCaloriesPerElf = [0, 0, 0];
-foreach ($inputs as $calories) {
-    if (strlen(trim($calories)) === 0) {
-        if ($currentCalories > min($totalCaloriesPerElf)) {
-            $totalCaloriesPerElf[] = $currentCalories;
-        }
+$totalCaloriesPerElf = [];
+foreach ($inputs as $key => $calories) {
+    $currentCalories += (int) $calories;
+
+    if (strlen(trim($calories)) === 0 || $key === $lastKey) {
+        $totalCaloriesPerElf = calculateCaloriesPerElf($totalCaloriesPerElf, $currentCalories);
 
         if ($topCalories < $currentCalories) {
             $topCalories = $currentCalories;
         }
 
         $currentCalories = 0;
-
-        continue;
     }
-
-    $currentCalories += (int) $calories;
 }
 
-echo $topCalories . PHP_EOL;
+function calculateCaloriesPerElf(array $totalCaloriesPerElf, int $currentCalories): array
+{
+    if (count($totalCaloriesPerElf) < 3) {
+        $totalCaloriesPerElf[$currentCalories] = $currentCalories;
+
+        return $totalCaloriesPerElf;
+    }
+
+    $minCalories = min($totalCaloriesPerElf);
+    if ($currentCalories > $minCalories) {
+        unset($totalCaloriesPerElf[$minCalories]);
+        $totalCaloriesPerElf[$currentCalories] = $currentCalories;
+    }
+
+    return $totalCaloriesPerElf;
+}
+
+echo 'part one: ' . $topCalories . PHP_EOL;
+echo 'part two: ' . array_sum($totalCaloriesPerElf) . PHP_EOL;
 
 
